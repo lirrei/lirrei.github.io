ae864cae28b89ca18cd6da0d8f0d6178cda07883
lirrei <166469624+lirrei@users.noreply.github.com>
2024-04-18 10:02:03 +0900

    Update homework1.py

--- 2024_spring/lec01/homework1.py
+++ 2024_spring/lec01/homework1.py
@@ -1,1 +1,19 @@
 M23W0553 liruihan
+This is the homework for the very first lecture of 
+Introduction to Speech Understanding.
+
+Your first homework is to change each of the two 
+following strings, then add the changes to your repository.
+commit the changes, push the changes to your github repo, 
+and then tell the instructor the URL for your
+github repo.
+
+
+
+stringl should be changed from "change this" to "test"
+string2 should be changed from "change this" to "hello"
+...
+
+string1 "test"
+
+string2 = "hello"
