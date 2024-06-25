ed85dfe244031107475bb059b8c9a9228e67a135
lirrei <166469624+lirrei@users.noreply.github.com>
2024-06-25 12:59:47 +0900

    Update homework8.py

--- 2024_spring/lec01/homework8.py
+++ 2024_spring/lec01/homework8.py
@@ -1,6 +1,10 @@
 import numpy as np
 
+
 def dft_matrix(N):
 
-    raise RuntimeError("You need to write this part")
-
+    W = np.zeros((N, N), dtype='complex')
+    for k in range(N):
+        for n in range(N):
+            W[k, n] = np.cos(2 * np.pi * k * n / N) - 1j * np.sin(2 * np.pi * k * n / N)
+    return W
